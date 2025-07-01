b9924263125df263104d9e92ae85e4e81989b653
David Muñoz Marin <166368229+KenMastters@users.noreply.github.com>
2025-07-01 11:47:28 +0200

    Update About.js

--- curri/src/componentes/About.js
+++ curri/src/componentes/About.js
@@ -6,11 +6,11 @@ const About = () => {
     <section className="about">
       <h2>Sobre mí</h2>
       <p>
-      Soy un desarrollador web junior titulado en Desarrollo de Aplicaciones Web por Comenius IDI.
+      Soy un desarrollador web junior titulado en Desarrollo de Aplicaciones Web por I.E.S Francisco de Goya.
         He completado mi formación, incluyendo la FCT, y he desarrollado un proyecto final consistente en una tienda de fútbol online llamada Futbol King, enfocada en la experiencia de usuario y el diseño responsive.
         Busco mi primera oportunidad laboral en el sector, en una empresa que valore el crecimiento continuo y el compromiso. 
-        Estoy especialmente interesado en seguir aprendiendo y profundizando en tecnologías como JavaScript, React, PHP, y otras que el entorno profesional requiera. 
-        Tengo una fuerte motivación, capacidad de adaptación y muchas ganas de aportar desde el primer día..
+        Estoy especialmente interesado en seguir aprendiendo y profundizando en tecnologías como JavaScript, React, PHP, WordPress y otras que el entorno profesional requiera. 
+        Tengo una fuerte motivación, capacidad de adaptación y muchas ganas de aportar desde el primer día.
       </p>
       <ul className="about-skills">
         <li>✔️ Proactivo y con gran capacidad de aprendizaje</li>
